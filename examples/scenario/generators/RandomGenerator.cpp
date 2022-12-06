047cd450b68cc9f19fc914ec17279c282cbf98e3
IanMelvin <ian.melvin@mymail.champlain.edu>
2022-12-05 22:03:14 -0500

    Terrain Gen Assignment

--- examples/scenario/generators/RandomGenerator.cpp
+++ examples/scenario/generators/RandomGenerator.cpp
@@ -18,10 +18,82 @@ std::vector<Color32> RandomScenarioGenerator::Generate(int sideSize, float displ
       float c1 = ((base.GetNoise((float)c,(float)l, displacement*50) +1)/2) *255;
       float c2 = ((cellular.GetNoise((float)c,(float)l, displacement*50) +1)/2) *255;
       auto avg = (c1+c2)/2;
-      colors.emplace_back(avg, avg, avg);
+      //colors.emplace_back(avg, avg, avg);
+      colors.emplace_back(Biomes(c1,c2,avg));
     }
   }
   std::cout<<colors.size() << std::endl;
-  return colors;
+  return colors;  // AltitudeFiltering(colors);
 }
 std::string RandomScenarioGenerator::GetName() { return "EXAMPLE"; }
+
+float RandomScenarioGenerator::HeightFiltering(
+    int noise, int x, int y, int sideSize) {
+  float nx = ((2 * x) / (sideSize)) - 1;
+  float ny = ((2 * y) / (sideSize)) - 1;
+  float distance = 1 - (1 - (nx * nx)) * (1 - (ny * ny));
+  float newElevation = (noise + (1 - (distance))) / 2;
+
+  return newElevation;
+}
+
+std::vector<Color32> RandomScenarioGenerator::AltitudeFiltering(
+    std::vector<Color32>& input) {
+  std::vector<Color32> modifiedColors;
+  for (Color32 var : input) {
+    if (var.r < waterLevel) {
+      modifiedColors.emplace_back(Color::Blue.Dark());
+    } else if (var.r < beachLevel) {
+      modifiedColors.emplace_back(Color::SandyBrown.Light());
+    } else {
+      modifiedColors.emplace_back(Color::Green.Light());
+    }
+  }
+  return modifiedColors;
+}
+
+
+
+Color32 RandomScenarioGenerator::Biomes(float x, float y, float avg) {
+  Biome biome;
+
+  if (avg < waterLevel) return GetColor(OCEAN);
+  if (avg < beachLevel) return GetColor(BEACH);
+
+  if (avg > 120) return GetColor(MOUNTAIN);
+
+  if (avg < 120) {
+    if (y < 20) {
+      return GetColor(DESERT);
+    } else if (y < 60) {
+      return GetColor(GRASSLAND);
+    }
+    else if (y < 100) {
+      return GetColor(FORREST);
+    }
+  }
+
+  biome = GRASSLAND;
+
+  return GetColor(biome);
+}
+
+Color32 RandomScenarioGenerator::GetColor(Biome biome) {
+  Color32 color;
+  switch (biome) {
+    case OCEAN:
+      return Color::Blue.Dark();
+    case BEACH:
+      return Color::SandyBrown.Light();
+    case GRASSLAND:
+      return Color::Green.Light();
+    case FORREST:
+      return Color::Green.Dark();
+    case DESERT:
+      return Color::SandyBrown.Light();
+    case MOUNTAIN:
+      return Color::Gray;
+    default:
+      return Color::Black;
+  }
+}
--- examples/scenario/generators/RandomGenerator.h
+++ examples/scenario/generators/RandomGenerator.h
@@ -5,8 +5,23 @@
 
 class RandomScenarioGenerator: public ScenarioGeneratorBase{
  public:
+  enum Biome {
+    OCEAN = 0,
+    BEACH = 1,
+    GRASSLAND = 2,
+    FORREST = 3,
+    DESERT = 4,
+    MOUNTAIN = 5
+  };
+
   std::vector<Color32> Generate(int sideSize, float displacement=0) override;
   std::string GetName() override;
+  float HeightFiltering(int noise, int x, int y, int sideSize);
+  std::vector<Color32> AltitudeFiltering(std::vector<Color32> &input);
+  Color32 Biomes(float x, float y, float avg);
+  Color32 GetColor(Biome biome);
+  int waterLevel = 90;
+  int beachLevel = 91;
 };
 
 #endif  // MOBAGEN_RANDOMSCENARIOGENERATOR_H
